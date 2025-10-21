3b555cc19b38472510118a07451b580538131315
Bryan Xol <xolbryan@gmail.com>
2025-10-20 20:07:41 -0600

    +rework

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './styles.css'
+import { SnackbarProvider } from 'notistack'
+import { AppThemeProvider } from './theme/ThemeContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppThemeProvider>
+      <SnackbarProvider maxSnack={4} autoHideDuration={3000} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </SnackbarProvider>
+    </AppThemeProvider>
+  </React.StrictMode>,
 )
